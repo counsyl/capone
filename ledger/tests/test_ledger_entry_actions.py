76c6b0474faee23127ab72e513cdd0c3bf7214e7
Hunter Richards <hunter@counsyl.com>
2015-12-18 10:59:49 -0800

    Add unit tests for 'credit' and 'debit'

--- ledger/tests/test_ledger_entry_actions.py
+++ ledger/tests/test_ledger_entry_actions.py
@@ -8,6 +8,8 @@ from django.utils.timezone import get_current_timezone
 from pytz import UTC
 
 from ledger.api.actions import Charge
+from ledger.api.actions import credit
+from ledger.api.actions import debit
 from ledger.api.actions import LedgerEntryAction
 from ledger.api.actions import Payment
 from ledger.api.actions import Refund
@@ -299,3 +301,27 @@ class TestRefundBalance(LedgerEntryActionSetUp):
         # receiving payments
         self.assertEqual(self.entity_1_rev_ledger.get_balance(),
                          -1 * self.charge_amount)
+
+
+class TestCreditAndDebit(TestCase):
+    AMOUNT = D(100)
+
+    def assertPositive(self, amount):
+        self.assertGreaterEqual(amount, 0)
+
+    def assertNegative(self, amount):
+        self.assertLess(amount, 0)
+
+    def test_credit_and_debit_helper_functions(self):
+        with mock.patch('ledger.api.actions.settings') as mock_settings:
+            mock_settings.DEBITS_ARE_NEGATIVE = True
+            self.assertPositive(credit(self.AMOUNT))
+            self.assertNegative(debit(self.AMOUNT))
+        with mock.patch('ledger.api.actions.settings') as mock_settings:
+            mock_settings.DEBITS_ARE_NEGATIVE = False
+            self.assertNegative(credit(self.AMOUNT))
+            self.assertPositive(debit(self.AMOUNT))
+
+    def test_validation_error(self):
+        self.assertRaises(ValueError, credit, -self.AMOUNT)
+        self.assertRaises(ValueError, debit, -self.AMOUNT)
