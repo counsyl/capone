247bbb310970ed187dd90b0f0ab02696dfc43d59
Hunter Richards <hunter@counsyl.com>
2015-12-18 11:14:35 -0800

    Regenerate migrations

--- ledger/migrations/0002_auto_20151218_1314.py
+++ ledger/migrations/0002_auto_20151218_1314.py
@@ -13,9 +13,14 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name='ledger',
-            name='are_debits_positive',
-            field=models.BooleanField(default=True, help_text=b'All accounts (and their corresponding ledgers) are of one of two types: either debits are positive and credits negative, or debits are negative and credits are positive.  By convention, asset and expense accounts are of the former type, while liabilities, equity, and revenue are of the latter.'),
+            name='increased_by_debits',
+            field=models.BooleanField(default=True, help_text=b'All accounts (and their corresponding ledgers) are of one of two types: either debits increase the value of an account or credits do.  By convention, asset and expense accounts are of the former type, while liabilities, equity, and revenue are of the latter.'),
             preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='transaction',
+            name='type',
+            field=models.CharField(default=b'Manual', max_length=128, verbose_name='The type of ledger', choices=[(b'Automatic', b'Automatic'), (b'Manual', b'Manual'), (b'Reconciliation', b'Reconciliation')]),
         ),
         migrations.AlterField(
             model_name='ledger',
@@ -31,5 +36,10 @@ class Migration(migrations.Migration):
             model_name='ledger',
             name='type',
             field=models.CharField(blank=True, max_length=128, verbose_name='The ledger type, eg Accounts Receivable, Revenue, etc', choices=[(b'ar', b'Accounts Receivable'), (b'revenue', b'Revenue'), (b'cash', b'Cash')]),
+        ),
+        migrations.AlterField(
+            model_name='ledgerentry',
+            name='amount',
+            field=models.DecimalField(verbose_name='Amount of this entry.', max_digits=24, decimal_places=4),
         ),
     ]
