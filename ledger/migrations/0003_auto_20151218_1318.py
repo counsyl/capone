b075f3a6b85250b4ddbf8464aa82729eb89c1e38
Hunter Richards <hunter@counsyl.com>
2015-12-18 11:18:16 -0800

    Redo migrations

--- ledger/migrations/0003_auto_20151218_1318.py
+++ ledger/migrations/0003_auto_20151218_1318.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ledger', '0003_auto_20151216_1544'),
+        ('ledger', '0002_auto_20151211_1501'),
     ]
 
     operations = [
@@ -20,5 +20,15 @@ class Migration(migrations.Migration):
             name='increased_by_debits',
             field=models.BooleanField(default=True, help_text=b'All accounts (and their corresponding ledgers) are of one of two types: either debits increase the value of an account or credits do.  By convention, asset and expense accounts are of the former type, while liabilities, equity, and revenue are of the latter.'),
             preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='transaction',
+            name='type',
+            field=models.CharField(default=b'Manual', max_length=128, verbose_name='The type of ledger', choices=[(b'Automatic', b'Automatic'), (b'Manual', b'Manual'), (b'Reconciliation', b'Reconciliation')]),
+        ),
+        migrations.AlterField(
+            model_name='ledgerentry',
+            name='amount',
+            field=models.DecimalField(verbose_name='Amount of this entry.', max_digits=24, decimal_places=4),
         ),
     ]
