944bd8129398f4e8ecc66dd180874424c17f1bc6
Hunter Richards <hunter@counsyl.com>
2016-02-05 15:54:56 -0800

    Add Ledger.description field

--- ledger/models.py
+++ ledger/models.py
@@ -362,6 +362,9 @@ class Ledger(NonDeletableModel, models.Model):
         _("Name of this ledger"),
         unique=True,
         max_length=255)
+    description = models.TextField(
+        help_text=_("Any notes to go along with this Transaction."),
+        blank=True)
     increased_by_debits = models.BooleanField(
         help_text="All accounts (and their corresponding ledgers) are of one of two types: either debits increase the value of an account or credits do.  By convention, asset and expense accounts are of the former type, while liabilities, equity, and revenue are of the latter.",  # nopep8
         default=None,
