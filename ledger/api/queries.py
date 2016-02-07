aa9fdc031cfc717ba2ed96c4bb078cb0b2472853
Hunter Richards <hunter@counsyl.com>
2016-02-07 13:26:59 -0800

    Add 'validate_transaction'

--- ledger/api/queries.py
+++ ledger/api/queries.py
@@ -40,3 +40,33 @@ def get_ledger_balances_for_transactions(transactions):
 def get_balances_for_object(obj):
     return get_ledger_balances_for_transactions(
         get_all_transactions_for_object(obj))
+
+
+def validate_transaction(
+    user,
+    evidence=(),
+    ledger_entries=(),
+    notes=None,
+    type=None,
+    posted_timestamp=None,
+):
+    """
+    Validates a Transaction and its sub-models before saving.
+
+    One, it validates that this Transaction properly balances.
+
+    A Transaction balances if its credit amounts match its debit amounts.
+    If the Transaction does not balance, then a TransactionBalanceException
+    is raised.
+
+    Two, it checks that the Transaction has entries in it.  We do not allow
+    empty Transactions.
+    """
+    total = sum([entry.amount for entry in ledger_entries])
+    if total != Decimal(0):
+        raise Transaction.TransactionBalanceException(
+            "Credits do not equal debits. Mis-match of %s." % total)
+
+    if not ledger_entries:
+        raise Transaction.NoLedgerEntriesException(
+            "Transaction has no entries.")
