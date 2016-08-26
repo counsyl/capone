18efc436837cf3db58b66d7c1ada8e8dee2267fa
Hunter Richards <hunter@counsyl.com>
2016-08-26 00:02:15 -0700

    Rewrite void tests

--- ledger/tests/test_transaction_actions.py
+++ ledger/tests/test_transaction_actions.py
@@ -1,24 +1,19 @@
 from datetime import datetime
 from decimal import Decimal as D
 
-from django.db import IntegrityError
 from django.test import TestCase
 
 from ledger.api.actions import create_transaction
 from ledger.api.actions import credit
-from ledger.api.actions import Charge
 from ledger.api.actions import debit
-from ledger.api.actions import Payment
-from ledger.api.actions import TransactionContext
-from ledger.api.actions import TransferAmount
 from ledger.api.actions import void_transaction
-from ledger.api.actions import WriteDown
 from ledger.models import Ledger
 from ledger.models import LedgerEntry
 from ledger.models import LEDGER_ACCOUNTS_RECEIVABLE
 from ledger.models import LEDGER_CASH
 from ledger.models import LEDGER_REVENUE
 from ledger.models import Transaction
+from ledger.tests.factories import TransactionFactory
 from ledger.tests.factories import UserFactory
 
 
@@ -40,13 +35,15 @@ class TestVoidTransaction(TestVoidBase):
     def test_simple_void(self):
         amount = D(100)
         # First record a charge
-        with TransactionContext(self.creation_user, self.creation_user) as txn:
-            txn.record_action(Charge(self.entity, amount))
+        txn = TransactionFactory(self.creation_user, ledger_entries=[
+            LedgerEntry(amount=debit(amount), ledger=self.entity_ar_ledger),
+            LedgerEntry(amount=credit(amount), ledger=self.entity_rev_ledger),
+        ])
 
         # Then void it
-        void_txn = void_transaction(txn.transaction, self.creation_user)
+        void_txn = void_transaction(txn, self.creation_user)
 
-        self.assertEqual(void_txn.voids, txn.transaction)
+        self.assertEqual(void_txn.voids, txn)
 
         self.assertEqual(self.entity_ar_ledger.get_balance(), D(0))
         self.assertEqual(self.entity_rev_ledger.get_balance(), D(0))
@@ -54,28 +51,32 @@ class TestVoidTransaction(TestVoidBase):
     def test_cant_void_twice(self):
         amount = D(100)
         # First record a charge
-        with TransactionContext(self.creation_user, self.creation_user) as txn:
-            txn.record_action(Charge(self.entity, amount))
+        txn = TransactionFactory(self.creation_user, ledger_entries=[
+            LedgerEntry(amount=debit(amount), ledger=self.entity_ar_ledger),
+            LedgerEntry(amount=credit(amount), ledger=self.entity_rev_ledger),
+        ])
 
         # Then void it
-        void_transaction(txn.transaction, self.creation_user)
+        void_transaction(txn, self.creation_user)
 
         # Trying to void the same transaction again will not succeed
         self.assertRaises(
             Transaction.UnvoidableTransactionException,
-            void_transaction, txn.transaction, self.creation_user)
+            void_transaction, txn, self.creation_user)
 
     def test_can_void_void(self):
         # A void transaction can be voided, thus re-instating the original txn
         amount = D(100)
         # First record a charge
-        with TransactionContext(self.creation_user, self.creation_user) as txn:
-            txn.record_action(Charge(self.entity, amount))
+        txn = TransactionFactory(self.creation_user, ledger_entries=[
+            LedgerEntry(amount=debit(amount), ledger=self.entity_ar_ledger),
+            LedgerEntry(amount=credit(amount), ledger=self.entity_rev_ledger),
+        ])
 
         # Then void it
-        void_txn = void_transaction(txn.transaction, self.creation_user)
+        void_txn = void_transaction(txn, self.creation_user)
 
-        self.assertEqual(void_txn.voids, txn.transaction)
+        self.assertEqual(void_txn.voids, txn)
 
         # And void the void
         void_void_txn = (void_transaction(void_txn, self.creation_user))
@@ -88,66 +89,22 @@ class TestVoidTransaction(TestVoidBase):
         amount_1 = D(100)
         amount_2 = D(200)
 
-        with TransactionContext(
-                self.creation_user, self.creation_user) as txn_1:
-            txn_1.record_action(Charge(self.entity, amount_1))
-        with TransactionContext(
-                self.creation_user, self.creation_user) as txn_2:
-            txn_2.record_action(Charge(self.entity, amount_2))
+        txn_1 = TransactionFactory(self.creation_user, ledger_entries=[
+            LedgerEntry(amount=debit(amount_1), ledger=self.entity_ar_ledger),
+            LedgerEntry(
+                amount=credit(amount_1), ledger=self.entity_rev_ledger),
+        ])
+        txn_2 = TransactionFactory(self.creation_user, ledger_entries=[
+            LedgerEntry(amount=debit(amount_2), ledger=self.entity_ar_ledger),
+            LedgerEntry(
+                amount=credit(amount_2), ledger=self.entity_rev_ledger),
+        ])
         self.assertNotEqual(txn_1, txn_2)
-        self.assertNotEqual(txn_1.transaction, txn_2.transaction)
 
-        void_transaction(txn_1.transaction, self.creation_user)
+        void_transaction(txn_1, self.creation_user)
 
         self.assertEqual(self.entity_ar_ledger.get_balance(), amount_2)
         self.assertEqual(self.entity_rev_ledger.get_balance(), -amount_2)
-
-    def test_can_void_cash(self):
-        # Cash transactions can be voided
-        amount = D(100)
-
-        with TransactionContext(
-                self.creation_user, self.creation_user) as charge_txn:
-            charge_txn.record_action(Charge(self.entity, amount))
-        with TransactionContext(
-                self.creation_user, self.creation_user) as pay_txn:
-            pay_txn.record_action(Payment(self.entity, amount))
-
-        # Void the charge
-        void_transaction(charge_txn.transaction, self.creation_user)
-        # And void the payment
-        void_transaction(pay_txn.transaction, self.creation_user)
-        self.assertEqual(self.entity_ar_ledger.get_balance(), D(0))
-        self.assertEqual(self.entity_rev_ledger.get_balance(), D(0))
-        self.assertEqual(self.entity_cash_ledger.get_balance(), D(0))
-
-    def test_void_combined_transaction(self):
-        # We can combine actions into a single transaction
-        # For example:
-        # 1. Charge $1000 to payer (Txn1)
-        # 2. Transfer $800 to PT (Txn2)
-        # 3. Comp PT $701 down to $99 (Txn2)
-        # 4. Refile claim, so undo transfer (Txn3)
-        #    Now Insurance owes $1000 and PT owes $0
-        charge_amount = D(1000)
-        transfer_amount = D(800)
-        comp_amount = D(701)
-
-        with TransactionContext(self.creation_user, self.creation_user) as txn:
-            txn.record_action(Charge(self.entity, charge_amount))
-
-        with TransactionContext(
-                self.creation_user, self.creation_user) as txn2:
-            txn2.record_action(TransferAmount(
-                self.entity, self.creation_user, transfer_amount))
-            txn2.record_action(WriteDown(self.creation_user, comp_amount))
-
-        self.assertEqual(self.creation_user_ar_ledger.get_balance(), D(99))
-        self.assertEqual(self.entity_ar_ledger.get_balance(), D(200))
-
-        void_transaction(txn2.transaction, self.creation_user)
-        self.assertEqual(self.creation_user_ar_ledger.get_balance(), D(0))
-        self.assertEqual(self.entity_ar_ledger.get_balance(), charge_amount)
 
     def test_void_from_create_transaction(self):
         amount = D(100)
@@ -161,13 +118,13 @@ class TestVoidTransaction(TestVoidBase):
                     amount=credit(amount),
                 ),
                 LedgerEntry(
-                    ledger=self.entity_cash_ledger,
+                    ledger=self.entity_rev_ledger,
                     amount=debit(amount),
                 ),
             ],
         )
         self.assertEqual(self.entity_ar_ledger.get_balance(), credit(amount))
-        self.assertEqual(self.entity_cash_ledger.get_balance(), debit(amount))
+        self.assertEqual(self.entity_rev_ledger.get_balance(), debit(amount))
         voiding_transaction = void_transaction(transaction, self.creation_user)
         self.assertEqual(
             set(tro.related_object for tro
@@ -175,7 +132,7 @@ class TestVoidTransaction(TestVoidBase):
             set(evidence),
         )
         self.assertEqual(self.entity_ar_ledger.get_balance(), D(0))
-        self.assertEqual(self.entity_cash_ledger.get_balance(), D(0))
+        self.assertEqual(self.entity_rev_ledger.get_balance(), D(0))
         self.assertEqual(voiding_transaction.voids, transaction)
         self.assertEqual(
             voiding_transaction.posted_timestamp,
@@ -199,7 +156,7 @@ class TestVoidTransaction(TestVoidBase):
                     amount=credit(amount),
                 ),
                 LedgerEntry(
-                    ledger=self.entity_cash_ledger,
+                    ledger=self.entity_rev_ledger,
                     amount=debit(amount),
                 ),
             ],
