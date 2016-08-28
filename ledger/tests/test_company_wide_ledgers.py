23a47c94eeef5e56e3fa3a316596b2ccab595a73
Hunter Richards <hunter@counsyl.com>
2016-08-27 22:04:34 -0700

    Move TestCreditAndDebit

--- ledger/tests/test_company_wide_ledgers.py
+++ ledger/tests/test_company_wide_ledgers.py
@@ -1,3 +1,4 @@
+import mock
 from datetime import datetime
 from decimal import Decimal as D
 
@@ -376,3 +377,30 @@ class TestExistingLedgerEntriesException(TestCase):
                 self.user,
                 ledger_entries=list(existing_transaction.entries.all()),
             )
+
+
+class TestCreditAndDebit(TestCase):
+    """
+    Test that `credit` and `debit` return the correctly signed amounts.
+    """
+    AMOUNT = D(100)
+
+    def assertPositive(self, amount):
+        self.assertGreaterEqual(amount, 0)
+
+    def assertNegative(self, amount):
+        self.assertLess(amount, 0)
+
+    def test_credit_and_debit_helper_functions(self):
+        with mock.patch('ledger.api.actions.settings') as mock_settings:
+            mock_settings.DEBITS_ARE_NEGATIVE = True
+            self.assertPositive(credit(self.AMOUNT))
+            self.assertNegative(debit(self.AMOUNT))
+        with mock.patch('ledger.api.actions.settings') as mock_settings:
+            mock_settings.DEBITS_ARE_NEGATIVE = False
+            self.assertNegative(credit(self.AMOUNT))
+            self.assertPositive(debit(self.AMOUNT))
+
+    def test_validation_error(self):
+        self.assertRaises(ValueError, credit, -self.AMOUNT)
+        self.assertRaises(ValueError, debit, -self.AMOUNT)
