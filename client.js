1622698674555256248de80e3a43c765295c438c
Hunter Richards <hunter@counsyl.com>
2020-04-03 15:09:42 -0500

    Fix perms

--- client.js
+++ client.js
@@ -3,9 +3,9 @@ const { Client } = require('pg');
 const pgclient = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres'
+    user: 'django',
+    password: 'django',
+    database: 'capone_test_db'
 });
 
 pgclient.connect();
