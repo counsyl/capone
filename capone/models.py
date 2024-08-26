7b8783d4bcc1abed88c039053654bce38233ddd0
Hunter Richards <hunter.richards@myriad.com>
2024-08-26 17:09:21 -0500

    Update deprecated import

--- capone/models.py
+++ capone/models.py
@@ -10,7 +10,7 @@ from django.contrib.contenttypes.fields import GenericRelation
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.db.models import Q
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from capone.exceptions import TransactionBalanceException
 
